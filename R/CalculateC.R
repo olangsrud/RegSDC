d30959112d104f3b435b52e3d78c22e523151269
Langsrud <oyl@ssb.no>
2018-12-29 23:55:02 +0100

    tidy_source

--- R/CalculateC.R
+++ R/CalculateC.R
@@ -1,15 +1,13 @@
 
-
-CalculateC = function(a, b, epsAlpha = 1e-07, AlphaHandler = warning){
-  if (is.null(epsAlpha))
+CalculateC <- function(a, b, epsAlpha = 1e-07, AlphaHandler = warning) {
+  if (is.null(epsAlpha)) 
     return(chol(t(a) %*% a - t(b) %*% b))
-  matrixC <- try(chol(t(a) %*% a - t(b) %*% b), silent=TRUE)
-  if(!inherits(matrixC,'try-error'))  
-    {
+  matrixC <- try(chol(t(a) %*% a - t(b) %*% b), silent = TRUE)
+  if (!inherits(matrixC, "try-error")) {
     attr(matrixC, "alpha") <- 1
     return(matrixC)
   }
-  alpha <- min(1, FindAlpha(a,b)/(1 + epsAlpha))  #
+  alpha <- min(1, FindAlpha(a, b)/(1 + epsAlpha))  #
   if (alpha < 1) 
     AlphaHandler(paste("alpha = ", alpha))
   
@@ -34,23 +32,26 @@ CalculateC = function(a, b, epsAlpha = 1e-07, AlphaHandler = warning){
 #'  
 #' @export
 #'
-FindAlpha = function(a,b){
-  aCol = IndependentCol(a)
-  bCol = IndependentCol(b)
-  cCol = aCol | bCol
-  if(any(!cCol)){ # common redundant columns removed
-    a = a[,cCol, drop=FALSE]
-    b = b[,cCol, drop=FALSE]
+FindAlpha <- function(a, b) {
+  aCol <- IndependentCol(a)
+  bCol <- IndependentCol(b)
+  cCol <- aCol | bCol
+  if (any(!cCol)) {
+    # common redundant columns removed
+    a <- a[, cCol, drop = FALSE]
+    b <- b[, cCol, drop = FALSE]
   }
-  ata = t(a)%*%a
-  btb = t(b)%*%b
-  if(!any(aCol!=cCol)) { # ata invertible 
-    m = solve(ata,btb)
-    return(sqrt(MinEigen(m,inverse=TRUE)))
+  ata <- t(a) %*% a
+  btb <- t(b) %*% b
+  if (!any(aCol != cCol)) {
+    # ata invertible
+    m <- solve(ata, btb)
+    return(sqrt(MinEigen(m, inverse = TRUE)))
   }
-  if(!any(bCol!=cCol)) { # btb invertible
-    m = solve(btb,ata)
-    return(sqrt(MinEigen(m,inverse=FALSE)))
+  if (!any(bCol != cCol)) {
+    # btb invertible
+    m <- solve(btb, ata)
+    return(sqrt(MinEigen(m, inverse = FALSE)))
   }
   stop("Could not calculate alpha.")
 }
@@ -58,26 +59,25 @@ FindAlpha = function(a,b){
 
 #' @rdname FindAlpha
 #' @export
-FindAlphaSimple = function(a,b){
+FindAlphaSimple <- function(a, b) {
   m <- t(a) %*% a %*% solve(t(b) %*% b)
   sqrt(min(eigen(m, only.values = TRUE)$values))
 }
 
-IndependentCol = function(x,tol = 1e-07){
-  a = rep(FALSE,NCOL(x))
-  qrX = qr(x,tol=tol)
-  a[qrX$pivot[seq_len(qrX$rank)]] =TRUE
+IndependentCol <- function(x, tol = 1e-07) {
+  a <- rep(FALSE, NCOL(x))
+  qrX <- qr(x, tol = tol)
+  a[qrX$pivot[seq_len(qrX$rank)]] <- TRUE
   a
 }
 
 # Use of tol inspierd by ginv i MASS
-MinEigen = function(x,tol = sqrt(.Machine$double.eps),inverse=TRUE,...){
-  a = Re(eigen(x,symmetric=FALSE, only.values = TRUE,...)$values)
-  if(inverse){
+MinEigen <- function(x, tol = sqrt(.Machine$double.eps), inverse = TRUE, ...) {
+  a <- Re(eigen(x, symmetric = FALSE, only.values = TRUE, ...)$values)
+  if (inverse) {
     ok <- abs(a) > max(tol * max(abs(a)), 0)
-    a = 1/a[ok]
+    a <- 1/a[ok]
   }
   min(a)
 }
-
 
