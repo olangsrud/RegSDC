a8efedf3b0cb0610b6becaebd3a5e3adff86c365
Langsrud <oyl@ssb.no>
2019-02-26 13:16:34 +0100

    SuppressDec examples

--- R/SuppressDec.R
+++ R/SuppressDec.R
@@ -14,6 +14,40 @@
 #' @export
 #'
 #' @examples
+#' # Same data as in the paper
+#' z <- RegSDCdata("sec7z")
+#' x <- RegSDCdata("sec7x")
+#' y <- RegSDCdata("sec7y")  # Now z is t(x) %*% y 
+#' zAll <- RegSDCdata("sec7zAll")
+#' zAllSupp <- RegSDCdata("sec7zAllSupp")
+#' xAll <- RegSDCdata("sec7xAll")
+#' 
+#' # When no suppression, output is identical to y
+#' SuppressDec(xAll, zAll, y)
+#' SuppressDec(xAll, zAll)  # y can be seen in z
+#' 
+#' # Similar to Y* in paper (but other random values)
+#' SuppressDec(x, z, y)
+#' 
+#' # Residual standard error forced to be 1
+#' SuppressDec(x, z, y, rmse = 1)
+#' 
+#' # Five ways of obtaining the same output
+#' SuppressDec(x, z, rmse = 1)  # slower, y must be estimated
+#' SuppressDec(xAll, zAllSupp, y, rmse = 1)
+#' SuppressDec(xAll, zAllSupp, rmse = 1)  # slower, y must be estimated
+#' SuppressDec(xAll, zAll, y, is.na(zAllSupp), rmse = 1)
+#' SuppressDec(xAll, zAll, suppressed = is.na(zAllSupp), rmse = 1)  # y seen in z
+#' 
+#' # YhatMod4 and YhatMod10 in Table 2 in paper
+#' SuppressDec(xAll, zAllSupp, y, yDeduct = 4 * (y%/%4), resScale = 0)
+#' SuppressDec(xAll, zAllSupp, y, yDeduct = 10 * (y%/%10), rmse = 0)
+#' 
+#' # As Table 3 in paper (but other random values)
+#' SuppressDec(xAll, zAllSupp, y, yDeduct = 10 * (y%/%10), resScale = 0.1)
+#' 
+#' # rmse instead of resScale and 5 draws
+#' SuppressDec(xAll, zAllSupp, y, yDeduct = 10 * (y%/%10), rmse = 1, nRep = 5)
 SuppressDec <- function(x, z, y = NULL, suppressed = NULL, digits = 9, nRep = 1, yDeduct = NULL, resScale = NULL, rmse = NULL) {
   origY <- !is.null(y)
   if (is.null(z)) 
@@ -65,9 +99,19 @@ SuppressDec <- function(x, z, y = NULL, suppressed = NULL, digits = 9, nRep = 1,
   }
   
   
-  if (any(!a$yKnown)) 
-    rw <- RoundWhole(IpsoExtra(y = a$y[which(!a$yKnown), , drop = FALSE], x = a$x, nRep = nRep, 
+  if (any(!a$yKnown)){ 
+    if(is.null(yDeduct)){
+      rw <- RoundWhole(IpsoExtra(y = a$y[which(!a$yKnown), , drop = FALSE], x = a$x, nRep = nRep, 
                                ensureIntercept = FALSE, rmse = rmse, resScale = resScale), digit = digits)
+    } else {
+      yDeduct <- EnsureMatrix(yDeduct[which(!a$yKnown)])
+      rw <- RoundWhole(IpsoExtra(y = a$y[which(!a$yKnown), , drop = FALSE] - yDeduct, x = a$x, nRep = nRep, 
+                                 ensureIntercept = FALSE, rmse = rmse, resScale = resScale), digit = digits)
+      if (nRep != 1)
+        yDeduct <- ColRepMatrix(yDeduct, nRep)
+      rw <- rw + yDeduct
+    }
+  }
   
   if (nRep != 1) 
     a$y <- ColRepMatrix(a$y, nRep)
