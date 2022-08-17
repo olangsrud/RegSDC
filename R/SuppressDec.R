a6dd4f9d8002a494b63f71a5e507410c4049ade6
Langsrud <oyl@ssb.no>
2022-08-17 10:05:59 +0200

    From SuppressDec in GaussSuppression package Code was copied to GaussSuppression and changed Now copy back GaussSuppression will import from RegSDC

--- R/SuppressDec.R
+++ R/SuppressDec.R
@@ -65,7 +65,7 @@
 #' 
 #' # rmse instead of resScale and 5 draws
 #' SuppressDec(xAll, zAllSupp, y, yDeduct = 10 * (y%/%10), rmse = 1, nRep = 5)
-SuppressDec <- function(x, z = NULL, y = NULL, suppressed = NULL, digits = 9, nRep = 1, yDeduct = NULL, resScale = NULL, rmse = NULL) {
+SuppressDec <- function(x, z = NULL, y = NULL, suppressed = NULL, digits = 9, nRep = 1, yDeduct = NULL, resScale = NULL, rmse = NULL, sparseLimit) {
   origY <- !is.null(y)
   if (!is.null(z)) 
     z <- EnsureMatrix(z, NCOL(x))
@@ -122,11 +122,11 @@ SuppressDec <- function(x, z = NULL, y = NULL, suppressed = NULL, digits = 9, nR
   if (any(!a$yKnown)){ 
     if(is.null(yDeduct)){
       rw <- RoundWhole(IpsoExtra(y = a$y[which(!a$yKnown), , drop = FALSE], x = a$x, nRep = nRep, 
-                               ensureIntercept = FALSE, rmse = rmse, resScale = resScale), digits = digits)
+                                 ensureIntercept = FALSE, rmse = rmse, resScale = resScale, sparseLimit = sparseLimit), digits = digits)
     } else {
       yDeduct <- EnsureMatrix(yDeduct)[which(!a$yKnown), , drop = FALSE]
       rw <- RoundWhole(IpsoExtra(y = a$y[which(!a$yKnown), , drop = FALSE] - yDeduct, x = a$x, nRep = nRep, 
-                                 ensureIntercept = FALSE, rmse = rmse, resScale = resScale), digits = digits)
+                                 ensureIntercept = FALSE, rmse = rmse, resScale = resScale, sparseLimit = sparseLimit), digits = digits)
       if (nRep != 1)
         yDeduct <- ColRepMatrix(yDeduct, nRep)
       rw <- rw + yDeduct
@@ -139,7 +139,6 @@ SuppressDec <- function(x, z = NULL, y = NULL, suppressed = NULL, digits = 9, nR
     a$y[which(!a$yKnown), ] <- rw
   a$y
 }
-
 
 
 
@@ -366,18 +365,42 @@ ReduceX <- function(x, z = NULL, y = NULL, digits = 9) {
 #' # Using data in the paper
 #' IpsoExtra(RegSDCdata("sec7y"), RegSDCdata("sec7x"))  # Similar to Y*
 #' IpsoExtra(RegSDCdata("sec7y"), RegSDCdata("sec7x"), rmse = 1)
-IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, nRep = 1, resScale = NULL, digits = 9, rmse = NULL) {
+IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, nRep = 1, resScale = NULL, digits = 9, rmse = NULL, sparseLimit, printInc = TRUE) {
+  
   y <- EnsureMatrix(y)
-  x <- EnsureMatrix(x, nrow(y))
-  if (ensureIntercept) 
-    x <- EnsureIntercept(x)
-  xQ <- GenQR(x, findR = FALSE)
   
-  #if (!is.null(rmse)) 
-  #  if (NCOL(y) > 1) 
-  #    stop("rmse parameter only when single y")
+  sparse <- (nrow(y) > sparseLimit) & (!is.matrix(x)) 
   
-  if (NROW(xQ) == NCOL(xQ)) {
+  if(sparse){
+    if(nrow(y) != nrow(x))
+      stop("nrow(y) != nrow(x)")
+    if (ensureIntercept)
+      stop("ensureIntercept not implemented when sparse")
+    dd <- DummyDuplicated(x, rnd = TRUE)
+    if (any(dd)) {
+      if (printInc) {
+        cat("-")
+        flush.console()
+      }
+      x <- x[ , !dd, drop = FALSE]
+    }
+    x <- x[ , GaussIndependent(x, printInc = printInc)[[2]], drop = FALSE]
+    if (printInc) {
+      cat("\n")
+    }
+    NROW_xQ <- nrow(x)
+    NCOL_xQ <- ncol(x)
+    crossprod_x <- crossprod(x) 
+  } else {
+    x <- EnsureMatrix(x, nrow(y))
+    if (ensureIntercept) 
+      x <- EnsureIntercept(x)
+    xQ <- GenQR(x, findR = FALSE)
+    NROW_xQ <- NROW(xQ)
+    NCOL_xQ <- NCOL(xQ)
+  }
+  
+  if (NROW_xQ == NCOL_xQ) {
     if (!is.null(resScale) | !is.null(rmse)) 
       warning("resScale/rmse ignored when Q from X is square.")
     if (nRep != 1) 
@@ -386,21 +409,27 @@ IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, 
       return(list(yHat = y, yRes = 0 * y)) else return(y)
   }
   
-  yHat <- xQ %*% (t(xQ) %*% y)
+  if(sparse){
+    yHat <- as.matrix(x %*% solve(crossprod_x, crossprod(x, y)))
+  } else {
+    yHat <- xQ %*% (t(xQ) %*% y)
+  }
   
   n <- NROW(y)
   ncoly <- NCOL(y)
   
   eQRR <- NULL
-  if (!is.null(digits) & !is.null(resScale)) {
+  if (!is.null(digits)) {   # if (!is.null(digits) & !is.null(resScale)) {  
     if (!is.null(rmse)) 
       if (max(abs(round(y - yHat, digits = digits))) == 0) {
-        if (ncoly > 1){
-          warning("rmse with identical residual vectors used instead of resScal since perfect fit.")
-        } else {
-          warning("rmse used instead of resScal since perfect fit.")
-        }
-        resScale <- NULL
+        if (!is.null(resScale)){  
+          if (ncoly > 1){
+            warning("rmse with identical residual vectors used instead of resScal since perfect fit.")
+          } else {
+            warning("rmse used instead of resScal since perfect fit.")
+          }
+          resScale <- NULL
+        }  
         eQRR <- matrix(1, 1, ncoly)  # Changed below
         m <- 1L
       }
@@ -416,7 +445,7 @@ IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, 
     if (ncoly > 1){ 
       rmseVar <- match(TRUE,!is.na(rmse))
       rmse <- rmse[rmseVar] 
-      resScale <- rmse * sqrt((n - NCOL(xQ))/sum(eQRR[, rmseVar]^2)) 
+      resScale <- rmse * sqrt((n - NCOL_xQ)/sum(eQRR[, rmseVar]^2)) 
     }
   
   
@@ -424,7 +453,7 @@ IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, 
     eQRR <- resScale * eQRR
   } else {
     if (!is.null(rmse)) 
-      eQRR[] <- sqrt(n - NCOL(xQ)) * rmse
+      eQRR[] <- sqrt(n - NCOL_xQ) * rmse
   }
   
   if (nRep != 1) {
@@ -433,7 +462,11 @@ IpsoExtra <- function(y, x = NULL, ensureIntercept = TRUE, returnParts = FALSE, 
   }
   for (i in seq_len(nRep)) {
     yNew <- matrix(rnorm(n * m), n, m)
-    eSim <- yNew - xQ %*% (t(xQ) %*% yNew)
+    if(sparse){
+      eSim <- yNew - as.matrix(x %*% solve(crossprod_x, crossprod(x, yNew)))
+    } else {
+      eSim <- yNew - xQ %*% (t(xQ) %*% yNew)
+    }
     eSimQ <- GenQR(eSim, findR = FALSE, makeunique = TRUE)
     if (nRep == 1) 
       yRes <- eSimQ %*% eQRR 
